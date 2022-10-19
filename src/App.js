8801bcf93634986a7b965fe415b33aadb30bd40a
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-10-20 04:08:25 +0900

    phase 2

--- src/App.js
+++ src/App.js
@@ -7,38 +7,40 @@ function App() {
   const cellB = [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 0, 1, 0]
   const cellD = [0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0]
   const cellC = [0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0]
-  const temp = 2
-  const totalFrame = Math.pow(16, temp * temp);
+  const unitNumber = 3
+  const totalFrame = Math.pow(16, unitNumber * unitNumber);
   const [currentFrame, setCurrentFrame] = useState(0)
   const [timerFlag, setTimerFlag] = useState(false)
   const [inputValue, setInputValue] = useState(1)
+  const [fps, setFps] = useState(100)
+  const [frequency, setFrequency] = useState(1)
   useEffect(() => {
     if (timerFlag) {
       var interval = setInterval(() => {
-        if (currentFrame === totalFrame + 1) setCurrentFrame(0)
-        else setCurrentFrame(currentFrame + 1)
-      }, 10);
+        if (currentFrame === totalFrame + frequency) setCurrentFrame(0)
+        else setCurrentFrame(currentFrame + frequency)
+      }, (1000 / fps));
     }
     else clearInterval(interval);
     return () => {
       clearInterval(interval);
     };
-  }, [currentFrame, timerFlag, totalFrame]);
+  }, [currentFrame, timerFlag, totalFrame, fps, frequency]);
   return (
     <div className="App">
       <div className='squares'>
         {
-          [...Array(temp)].map((item, firstIndex) => {
+          [...Array(unitNumber)].map((item, firstIndex) => {
             return (
               <div className='row' key={"first" + firstIndex}>
                 {
-                  [...Array(temp)].map((item, secondIndex) => {
+                  [...Array(unitNumber)].map((item, secondIndex) => {
 
-                    const currentCell = (temp * temp) - parseInt(firstIndex.toString() + secondIndex.toString(), 2)
+                    const currentCell = (unitNumber * unitNumber) - parseInt(firstIndex.toString() + secondIndex.toString(), unitNumber)
                     const sixteenNumber = currentFrame.toString(16)
                     const index = parseInt(sixteenNumber[sixteenNumber.length - currentCell], 16) || 0
-                    console.log(firstIndex, secondIndex, currentCell,
-                      currentFrame, sixteenNumber, sixteenNumber[currentCell], parseInt(sixteenNumber[currentCell], 16) || 0)
+                    // console.log(firstIndex, secondIndex, currentCell,
+                    //   currentFrame, sixteenNumber, sixteenNumber[currentCell], parseInt(sixteenNumber[currentCell], 16) || 0)
 
                     return (
                       <div className='unit-squares' key={"first" + secondIndex}>
@@ -66,21 +68,33 @@ function App() {
       </div>
       <div className='current-frame'>Total Frame : {totalFrame}</div>
       <div className='current-frame'>Current Frame : {currentFrame + 1}</div>
+      <div className='sliders'>
+        <div className='slider-box'>
+          <Slider valueLabelDisplay="auto" value={fps} onChange={(e) => { setFps(e.target.value) }} step={1} min={1} max={1000} />
+          <p>Speed FPS : {fps}</p>
+        </div>
+        <div className='slider-box'>
+          <Slider valueLabelDisplay="auto" value={frequency} onChange={(e) => { setFrequency(e.target.value) }} step={1} min={1} max={1000000000} />
+          <p>Frequency : {frequency}</p>
+        </div>
+      </div>
       <div className='go-to-frame'>
         <p>Go To Frame</p>
         <input type={'number'} max={totalFrame} value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-        <div className='button' onClick={() => { setCurrentFrame(inputValue) }}>Go</div>
+        <div className='button' onClick={() => { setCurrentFrame(parseInt(inputValue - 1)) }}>Go</div>
       </div>
       <div className='slider'>
-        <Slider valueLabelDisplay="auto" value={currentFrame+1} onChange={(e)=>{
+        <Slider valueLabelDisplay="auto" value={currentFrame + 1} onChange={(e) => {
           setCurrentFrame(e.target.value)
-        }} step={1} min={1} max={totalFrame}/>
+        }} step={1} min={1} max={totalFrame} />
       </div>
       <div className='reset-all'>
         <div className='button' onClick={() => {
           setCurrentFrame(0);
           setInputValue(0);
           setTimerFlag(false);
+          setFps(100)
+          setFrequency(1)
         }}>Reset All</div>
       </div>
     </div>
