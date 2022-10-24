92a1e967ab4c31afdb155bb5ffb69ba90d13c28d
LovelyDev829 <lovelydeveloper829@gmail.com>
2022-10-25 05:03:20 +0900

    draw mode

--- src/App.js
+++ src/App.js
@@ -21,16 +21,15 @@ function App() {
   const [backwardFlag, setBackwardFlag] = useState(false)
   const [stepForwardFlag, setStepForwardFlag] = useState(false)
   const [stepBackwardFlag, setStepBackwardFlag] = useState(false)
+  const [drawFlag, setDrawFlag] = useState(false)
 
   useEffect(() => {
     var interval
     if (timerFlag) {
       interval = setInterval(() => {
         var times = (fps / shutterFps).toFixed()
-        console.log("times", times)
         var tempCurrentFrame = currentFrame
         while (times--) {
-          console.log("times--", times)
           if (tempCurrentFrame.plus(frequency).comparedTo(totalFrame) === 1) tempCurrentFrame = tempCurrentFrame.plus(frequency).minus(totalFrame)
           else tempCurrentFrame = tempCurrentFrame.plus(frequency)
         }
@@ -45,7 +44,6 @@ function App() {
 
   useEffect(() => {
     var timer, tempCurrentFrame
-    // console.log(currentFrame)
     if (forwardFlag) {
       timer = setTimeout(() => {
         tempCurrentFrame = currentFrame.plus(frequency)
@@ -84,7 +82,8 @@ function App() {
     return () => clearTimeout(timer)
   })
   return (
-    <div className="App">
+    <div className={drawFlag ? 'App curser' : 'App'}>
+      <div className={drawFlag ? 'button draw-mode' : 'button draw-mode disabled'} onClick={() => setDrawFlag(!drawFlag)}>DRAW MODE</div>
       <div className='squares'>
         {
           [...Array(unitNumber)].map((item, firstIndex) => {
@@ -94,18 +93,57 @@ function App() {
                   [...Array(unitNumber)].map((item, secondIndex) => {
 
                     const currentCell = (unitNumber * unitNumber) - parseInt(firstIndex.toString() + secondIndex.toString(), unitNumber)
-                    const sixteenNumber = currentFrame.toString(16)
-                    // console.log(currentFrame.toFixed(), sixteenNumber)
+                    var sixteenNumber = currentFrame.toString(16)
                     const index = parseInt(sixteenNumber[sixteenNumber.length - currentCell], 16) || 0
                     return (
                       <div className='unit-squares' key={"first" + secondIndex}>
                         <div className='row'>
-                          <div className={cellA[index] === 1 ? 'square black' : 'square'}></div>
-                          <div className={cellB[index] === 1 ? 'square black' : 'square'}></div>
+                          <div className={cellA[index] === 1 ? 'square black' : 'square'} onClick={() => {
+                            if(!drawFlag) return;
+                            [...Array(unitNumber * unitNumber)].forEach((item, tempIndex) => {
+                              if (cellA[index] !== cellA[tempIndex] && cellB[index] === cellB[tempIndex] && cellC[index] === cellC[tempIndex] && cellD[index] === cellD[tempIndex]) {
+                                const chIndex = sixteenNumber.length - currentCell
+                                const newSixteenNumber = sixteenNumber.substring(0, chIndex) + tempIndex.toString(16) + sixteenNumber.substring(chIndex + 1)
+                                const newCurrentFrame = new BigNumber(newSixteenNumber, 16)
+                                setCurrentFrame(newCurrentFrame)
+                              }
+                            })
+                          }}></div>
+                          <div className={cellB[index] === 1 ? 'square black' : 'square'} onClick={() => {
+                            if(!drawFlag) return;
+                            [...Array(unitNumber * unitNumber)].forEach((item, tempIndex) => {
+                              if (cellA[index] === cellA[tempIndex] && cellB[index] !== cellB[tempIndex] && cellC[index] === cellC[tempIndex] && cellD[index] === cellD[tempIndex]) {
+                                const chIndex = sixteenNumber.length - currentCell
+                                const newSixteenNumber = sixteenNumber.substring(0, chIndex) + tempIndex.toString(16) + sixteenNumber.substring(chIndex + 1)
+                                const newCurrentFrame = new BigNumber(newSixteenNumber, 16)
+                                setCurrentFrame(newCurrentFrame)
+                              }
+                            })
+                          }}></div>
                         </div>
                         <div className='row'>
-                          <div className={cellC[index] === 1 ? 'square black' : 'square'}></div>
-                          <div className={cellD[index] === 1 ? 'square black' : 'square'}></div>
+                          <div className={cellC[index] === 1 ? 'square black' : 'square'} onClick={() => {
+                            if(!drawFlag) return;
+                            [...Array(unitNumber * unitNumber)].forEach((item, tempIndex) => {
+                              if (cellA[index] === cellA[tempIndex] && cellB[index] === cellB[tempIndex] && cellC[index] !== cellC[tempIndex] && cellD[index] === cellD[tempIndex]) {
+                                const chIndex = sixteenNumber.length - currentCell
+                                const newSixteenNumber = sixteenNumber.substring(0, chIndex) + tempIndex.toString(16) + sixteenNumber.substring(chIndex + 1)
+                                const newCurrentFrame = new BigNumber(newSixteenNumber, 16)
+                                setCurrentFrame(newCurrentFrame)
+                              }
+                            })
+                          }}></div>
+                          <div className={cellD[index] === 1 ? 'square black' : 'square'} onClick={() => {
+                            if(!drawFlag) return;
+                            [...Array(unitNumber * unitNumber)].forEach((item, tempIndex) => {
+                              if (cellA[index] === cellA[tempIndex] && cellB[index] === cellB[tempIndex] && cellC[index] === cellC[tempIndex] && cellD[index] !== cellD[tempIndex]) {
+                                const chIndex = sixteenNumber.length - currentCell
+                                const newSixteenNumber = sixteenNumber.substring(0, chIndex) + tempIndex.toString(16) + sixteenNumber.substring(chIndex + 1)
+                                const newCurrentFrame = new BigNumber(newSixteenNumber, 16)
+                                setCurrentFrame(newCurrentFrame)
+                              }
+                            })
+                          }}></div>
                         </div>
                       </div>
                     )
@@ -122,6 +160,47 @@ function App() {
         <div className='button' onClick={() => { setCurrentFrame(new BigNumber(0)); }}>Reset</div>
       </div>
       <div className='current-frame'>Total Frame : {totalFrame.toFixed()}</div>
+      <div className='slider'>
+        <div className='button' onMouseDown={() => setBackwardFlag(true)} onClick={() => {
+          var tempCurrentFrame = currentFrame.minus(frequency)
+          if (tempCurrentFrame.comparedTo(0) === -1) setCurrentFrame(tempCurrentFrame.plus(totalFrame))
+          else setCurrentFrame(tempCurrentFrame)
+        }}
+          onMouseLeave={() => setBackwardFlag(false)} onMouseUp={() => setBackwardFlag(false)}>{'<<<'}</div>
+
+        <div className='button left' onMouseDown={() => setStepBackwardFlag(true)}
+          onMouseLeave={() => setStepBackwardFlag(false)} onMouseUp={() => setStepBackwardFlag(false)}
+          onClick={() => {
+            var tempCurrentFrame = currentFrame.minus(1)
+            if (tempCurrentFrame.comparedTo(0) === -1) setCurrentFrame(totalFrame)
+            else setCurrentFrame(tempCurrentFrame)
+          }}>{'<'}</div>
+
+        <Slider step={1} min={1} max={MAX_NUM} value={parseInt(currentFrame.dividedBy(totalFrame).multipliedBy(MAX_NUM).toFixed())} onChange={(e) => {
+          setCurrentFrame(totalFrame.dividedBy(MAX_NUM - 1).multipliedBy(e.target.value - 1).integerValue(BigNumber.ROUND_FLOOR))
+        }} />
+
+        <div className='button right' onMouseDown={() => setStepForwardFlag(true)}
+          onMouseLeave={() => setStepForwardFlag(false)} onMouseUp={() => setStepForwardFlag(false)}
+          onClick={() => {
+            var tempCurrentFrame = currentFrame.plus(1)
+            if (tempCurrentFrame.comparedTo(totalFrame) === 1) setCurrentFrame(new BigNumber(0))
+            else setCurrentFrame(tempCurrentFrame)
+          }}>{'>'}</div>
+
+        <div className='button' onMouseDown={() => setForwardFlag(true)} onClick={() => {
+          var tempCurrentFrame = currentFrame.plus(frequency)
+          if (tempCurrentFrame.comparedTo(totalFrame) === 1) setCurrentFrame(tempCurrentFrame.minus(totalFrame))
+          else setCurrentFrame(tempCurrentFrame)
+        }}
+          onMouseLeave={() => setForwardFlag(false)} onMouseUp={() => setForwardFlag(false)}>{'>>>'}</div>
+      </div>
+      <div className='current-frame'>Current Frame : {currentFrame.plus(1).toFixed()}</div>
+      <div className='go-to-frame'>
+        <p>Go To Frame</p>
+        <input type={'text'} min={1} value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
+        <div className='button' onClick={() => { setCurrentFrame(new BigNumber(inputValue).minus(1)) }}>Go</div>
+      </div>
       <div className='sliders'>
         <div className='slider-box'>
           <Slider value={fps} onChange={(e) => {
@@ -163,47 +242,6 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='go-to-frame'>
-        <p>Go To Frame</p>
-        <input type={'text'} min={1} value={inputValue} onChange={(e) => { setInputValue(e.target.value) }} />
-        <div className='button' onClick={() => { setCurrentFrame(new BigNumber(inputValue).minus(1)) }}>Go</div>
-      </div>
-      <div className='slider'>
-        <div className='button' onMouseDown={() => setBackwardFlag(true)} onClick={() => {
-          var tempCurrentFrame = currentFrame.minus(frequency)
-          if (tempCurrentFrame.comparedTo(0) === -1) setCurrentFrame(tempCurrentFrame.plus(totalFrame))
-          else setCurrentFrame(tempCurrentFrame)
-        }}
-          onMouseLeave={() => setBackwardFlag(false)} onMouseUp={() => setBackwardFlag(false)}>{'<<<'}</div>
-
-        <div className='button left' onMouseDown={() => setStepBackwardFlag(true)}
-          onMouseLeave={() => setStepBackwardFlag(false)} onMouseUp={() => setStepBackwardFlag(false)}
-          onClick={() => {
-            var tempCurrentFrame = currentFrame.minus(1)
-            if (tempCurrentFrame.comparedTo(0) === -1) setCurrentFrame(totalFrame)
-            else setCurrentFrame(tempCurrentFrame)
-          }}>{'<'}</div>
-
-        <Slider step={1} min={1} max={MAX_NUM} value={parseInt(currentFrame.dividedBy(totalFrame).multipliedBy(MAX_NUM).toFixed())} onChange={(e) => {
-          setCurrentFrame(totalFrame.dividedBy(MAX_NUM - 1).multipliedBy(e.target.value - 1).integerValue(BigNumber.ROUND_FLOOR))
-        }} />
-
-        <div className='button right' onMouseDown={() => setStepForwardFlag(true)}
-          onMouseLeave={() => setStepForwardFlag(false)} onMouseUp={() => setStepForwardFlag(false)}
-          onClick={() => {
-            var tempCurrentFrame = currentFrame.plus(1)
-            if (tempCurrentFrame.comparedTo(totalFrame) === 1) setCurrentFrame(new BigNumber(0))
-            else setCurrentFrame(tempCurrentFrame)
-          }}>{'>'}</div>
-
-        <div className='button' onMouseDown={() => setForwardFlag(true)} onClick={() => {
-          var tempCurrentFrame = currentFrame.plus(frequency)
-          if (tempCurrentFrame.comparedTo(totalFrame) === 1) setCurrentFrame(tempCurrentFrame.minus(totalFrame))
-          else setCurrentFrame(tempCurrentFrame)
-        }}
-          onMouseLeave={() => setForwardFlag(false)} onMouseUp={() => setForwardFlag(false)}>{'>>>'}</div>
-      </div>
-      <div className='current-frame'>Current Frame : {currentFrame.plus(1).toFixed()}</div>
       <div className='reset-all'>
         <div className='button' onClick={() => {
           setCurrentFrame(new BigNumber(0));
